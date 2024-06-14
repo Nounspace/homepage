d294ffd07f85dee70c44ab7dd770d517be1a8c8f
Gleb Surinov <glebovsky@gmail.com>
2024-06-14 10:17:38 +0600

    upd faqs

--- src/app/faq/nounspace/page.tsx
+++ src/app/faq/nounspace/page.tsx
@@ -1,15 +1,32 @@
-'use client'
+"use client";
 import CollapsibleFaqs from "@/components/pages/faq/CollapsibleFaqs";
 import NavigationSlider from "@/components/pages/faq/NavigationSlider";
 
 const questions = [
   {
     question: "What is Nounspace?",
-    answer: "asdf",
+    answer:
+      "Nounspace is a customizable social media platform built on the Farcaster protocol. It allows users to create profiles, customize their spaces with themes and fidgets (miniature applications), explore a diverse spectrum of bespoke spaces, and participate in community-governance to shape the future of social media.",
   },
   {
-    question: "How do I join Nounspace?",
-    answer: "asdf",
+    question: "What is the Nounspace Fair Launch?",
+    answer:
+      "The Nounspace Fair Launch is a decentralized event where participants can deposit their stETH and earn $SPACE. These tokens provide governance rights within the Nounspace ecosystem, a new social media platform built on the Farcaster protocol.",
+  },
+  {
+    question: "When does Nounspace launch?",
+    answer:
+      "On July 2 at 1:11pm UTC, Nounspace will launch. Anyone with an internet connection will be able to visit the space of any farcaster user, and all nOGs holders will have access to customize their space and homebase.",
+  },
+  {
+    question: "What are $SPACE tokens?",
+    answer:
+      "$SPACE tokens are governance tokens earned through the Nounspace Fair Launch. They provide holders with voting rights and a say in the governance and future direction of the Nounspace platform. Governance is the core utility of $SPACE. From there, the community can extend the powers of $SPACE infinitely.",
+  },
+  {
+    question: "What are nOGs?",
+    answer:
+      "nOGs are Nounspace OG NFTs. These NFTs feature hand-drawn frames by @latsko.eth, masked over 1,011 different pieces of CC0 artwork and photography. Minting nOGs grants the earliest access to customize your space on Nounspace and free premium access for life.",
   },
 ];
 
--- src/app/faq/space/page.tsx
+++ src/app/faq/space/page.tsx
@@ -1,15 +1,31 @@
-'use client'
+"use client";
 import CollapsibleFaqs from "@/components/pages/faq/CollapsibleFaqs";
 import NavigationSlider from "@/components/pages/faq/NavigationSlider";
 
 const questions = [
   {
-    question: "What is Nounspace?",
-    answer: "asdf",
+    question: "What is the Nounspace Fair Launch?",
+    answer:
+      "The Nounspace Fair Launch is a decentralized event where participants can deposit their stETH and earn $SPACE. These tokens provide governance rights within the Nounspace ecosystem, a new social platform built on the Farcaster protocol.",
   },
   {
-    question: "How do I join Nounspace?",
-    answer: "asdf",
+    question: "How do I participate in the Nounspace Fair Launch?",
+    answer:
+      "To participate, you need to stake ETH by converting it to stETH via [Lido](https://stake.lido.fi/). Next, deposit your stETH into the Nounspace contract on the Ethereum mainnet through the [$SPACE dashboard](https://www.notion.so/Living-Scope-Doc-e348b1708ccf4f3f8d866460dd74e830?pvs=21).",
+  },
+  {
+    question: "Do I need to have stETH to participate?",
+    answer:
+      "Yes, you need stETH to participate. If you don't have stETH, you can convert your ETH to stETH using [Lido](https://stake.lido.fi/) before depositing it into the Nounspace contract.",
+  },
+  {
+    question: "Where can I find the Nounspace staking dashboard?",
+    answer:
+      "The [$SPACE dashboard](https://www.notion.so/Nounspace-Tom-633aad1739394f28b8057f5d9a57ce9c?pvs=21) is available on the Nounspace website. It provides instructions and a user-friendly interface to guide you through the staking process.",
+  },
+  {
+    question: "What is the minimum amount of stETH required to participate?",
+    answer: "You must stake at least 0.01 stETH per deposit. There is no max.",
   },
 ];
 
