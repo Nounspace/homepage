b51d5a465b51dcc62e72fe1f50d776e522dda7f4
Gleb Surinov <glebovsky@gmail.com>
2024-06-14 21:53:56 +0600

    Links in FAQ

--- src/components/pages/faq/CollapsibleFaqs.tsx
+++ src/components/pages/faq/CollapsibleFaqs.tsx
@@ -1,19 +1,19 @@
-'use client'
+"use client";
 import React from "react";
 import { Box, Collapse, Flex } from "@chakra-ui/react";
 import { MdExpandMore, MdExpandLess } from "react-icons/md";
 import { useState } from "react";
-import Markdown from 'react-markdown';
+import Markdown from "react-markdown";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 export type FaqQuestion = {
-    question: string;
-    answer: string;
-}
+  question: string;
+  answer: string;
+};
 
 export type CollapsibleFaqsProps = {
-    questions: FaqQuestion[];
-}
+  questions: FaqQuestion[];
+};
 
 const CollapsibleFaqs: React.FC<CollapsibleFaqsProps> = ({ questions }) => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -35,8 +35,21 @@ const CollapsibleFaqs: React.FC<CollapsibleFaqsProps> = ({ questions }) => {
 
   return (
     <ChakraProvider theme={faqMarkdownStyles}>
-      <Box borderRadius="2xl" border="1px solid" borderColor="gray.300" p="4" bg="gray.100" mt="4">
-        <Flex direction="column" border="1px solid" borderColor="gray.300" borderRadius="lg" overflow="hidden">
+      <Box
+        borderRadius="2xl"
+        border="1px solid"
+        borderColor="gray.300"
+        p="4"
+        bg="gray.100"
+        mt="4"
+      >
+        <Flex
+          direction="column"
+          border="1px solid"
+          borderColor="gray.300"
+          borderRadius="lg"
+          overflow="hidden"
+        >
           {questions.map((faq, index) => (
             <Box
               key={index}
@@ -55,10 +68,19 @@ const CollapsibleFaqs: React.FC<CollapsibleFaqsProps> = ({ questions }) => {
                 {openIndex === index ? <MdExpandLess /> : <MdExpandMore />}
               </Flex>
               <Collapse in={openIndex === index} animateOpacity>
-                <Box p={3} mt={2} borderRadius="lg" bg="gray.100">
-                  <Markdown>
-                    {faq.answer}
-                  </Markdown>
+                <Box
+                  p={3}
+                  mt={2}
+                  borderRadius="lg"
+                  bg="gray.100"
+                  sx={{
+                    a: {
+                      textDecoration: "underline",
+                      color: "purple.500",
+                    },
+                  }}
+                >
+                  <Markdown>{faq.answer}</Markdown>
                 </Box>
               </Collapse>
             </Box>
