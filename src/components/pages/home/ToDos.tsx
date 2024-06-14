5fe5d4e8e84bb8f7ac96cc9b9186817cbf742a84
Gleb Surinov <glebovsky@gmail.com>
2024-06-14 09:59:29 +0600

    todos links

--- src/components/pages/home/ToDos.tsx
+++ src/components/pages/home/ToDos.tsx
@@ -1,24 +1,33 @@
 "use client";
 
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/next-js";
+import { Box, Flex } from "@chakra-ui/react";
 import { Checkbox } from "@chakra-ui/react";
 
 const ToDos = () => {
   return (
     <Box>
       <Flex gap="2">
-        <Checkbox colorScheme="purple">
-          <Text fontWeight="semibold" textDecor="underline" color="purple.500">
-            mint nOGs
-          </Text>
-        </Checkbox>
+        <Checkbox colorScheme="purple"></Checkbox>
+        <Link
+          href="/nogs/mint"
+          fontWeight="semibold"
+          textDecor="underline"
+          color="purple.500"
+        >
+          mint nOGs
+        </Link>
       </Flex>
       <Flex gap="2">
-        <Checkbox colorScheme="purple">
-          <Text fontWeight="semibold" textDecor="underline" color="purple.500">
-            participate in fair launch
-          </Text>
-        </Checkbox>
+        <Checkbox colorScheme="purple"></Checkbox>
+        <Link
+          href="/faq/nounspace"
+          fontWeight="semibold"
+          textDecor="underline"
+          color="purple.500"
+        >
+          participate in fair launch
+        </Link>
       </Flex>
     </Box>
   );
