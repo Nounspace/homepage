0a5ad0c6510ded411a4f9d22b7398592cd2af56a
alelliott <alexjmelliott@gmail.com>
2024-06-20 11:31:47 -0400

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { globalMetadata } from "@/app/metadata";
 import Providers from "@/app/providers";
+import Script from 'next/script'
 
 import { Box, Grid, Container } from "@chakra-ui/react";
 import Navigation from "@/components/Navigation";
@@ -11,7 +12,22 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     // `suppressHydrationWarning` is used to suppress the warning about adding additional attributes (from libs like Chakra UI) to the `html` and `body` tags after page hydration
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KBH80MT59R"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KBH80MT59R');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning={true}>
+        
         <Providers>
           <Navigation />
           <Box as="main" py={{ base: 4, md: 0 }} pb={{ base: 24, md: 0 }}>
