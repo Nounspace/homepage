f063fbfa08eb83d32576410a55cb80b94ed76b56
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-27 12:59:57 -0500

    Update Nounspace FAQ

--- src/app/faq/nounspace/page.tsx
+++ src/app/faq/nounspace/page.tsx
@@ -7,27 +7,32 @@ const questions = [
     question: "What is Nounspace?",
 
     answer:
-      "Nounspace is a customizable social media platform built on the Farcaster protocol. It allows users to create profiles, customize their spaces with themes and fidgets (miniature applications), explore a diverse spectrum of bespoke spaces, and participate in community-governance to shape the future of social media.",
+      "[Nounspace](https://nounspace.com) is a customizable social media platform built on [Farcaster](https://docs.farcaster.xyz/), a sufficiently decentralized social network. It allows users to create profile and dashboard spaces, customize their spaces with themes and fidgets (mini apps), explore customized spaces, and participate in community-governance to shape the future of social.",
   },
   {
-    question: "What is the Nounspace Fair Launch?",
+    question: "Who can use Nounspace? Do I need a Nouns NFT?",
     answer:
-      "The Nounspace Fair Launch is a decentralized event where participants can deposit their stETH and earn $SPACE. These tokens provide governance rights within the Nounspace ecosystem, a new social media platform built on the Farcaster protocol.",
+      "Anyone can use Nounspace! No NFT required. All you need is a wallet and a Farcaster account.",
   },
   {
-    question: "When does Nounspace launch?",
+    question: "What is Nounspace's relationship to Nouns?",
     answer:
-      "On July 2 at 1:11pm UTC, Nounspace will launch. Anyone with an internet connection will be able to visit the space of any farcaster user, and all nOGs holders will have access to customize their space and homebase.",
+      "Nounspace was initially [funded by Nouns DAO](https://prop.house/0x767a3bdf2aa3b3201b794927a997fcf4e50d4702/21) and is focused on the Nouns community and ecosystem. However, we're working on enabling any community to easily deploy their own whitelabeled version of Nounspace. To be notified when this is live, please [fill out this form](https://app.deform.cc/form/625acfaf-5d14-4d48-af5f-c8cf521342ef/).",
   },
   {
     question: "What are $SPACE tokens?",
     answer:
-      "$SPACE tokens are governance tokens earned through the Nounspace Fair Launch. They provide holders with voting rights and a say in the governance and future direction of the Nounspace platform. Governance is the core utility of $SPACE. From there, the community can extend the powers of $SPACE infinitely.",
+      "[$SPACE](https://basescan.org/address/0x48C6740BcF807d6C47C864FaEEA15Ed4dA3910Ab) tokens are governance token for Nounspace and can be earned through the Nounspace Fair Launch. They provide holders with voting rights and a say in the governance and future direction of the Nounspace platform. Governance is the core utility of $SPACE. From there, the community can extend the powers of $SPACE infinitely. Learn more about $SPACE and participate in the fair launch on the [$SPACE token space](https://www.nounspace.com/t/base/0x48C6740BcF807d6C47C864FaEEA15Ed4dA3910Ab/Profile).",
+  },
+  {
+    question: "What is the Nounspace Fair Launch?",
+    answer:
+      "The Nounspace Fair Launch is a decentralized event where participants can deposit their stETH and earn $SPACE. These tokens provide governance rights within the Nounspace ecosystem.",
   },
   {
     question: "What are nOGs?",
     answer:
-      "nOGs are Nounspace OG NFTs. These NFTs feature hand-drawn frames by @latsko.eth, masked over 1,011 different pieces of CC0 artwork and photography. Minting nOGs grants the earliest access to customize your space on Nounspace and free premium access for life.",
+      "nOGs are Nounspace OG NFTs. These NFTs feature hand-drawn frames by @latsko.eth, masked over 1,011 different pieces of CC0 artwork and photography. [Minting nOGs](https://highlight.xyz/mint/base:0xD094D5D45c06c1581f5f429462eE7cCe72215616) grants early access to new features and free premium access for life.",
 
   },
 ];
