5a524ec3eeced9509f855cbe739dd545dc49e73a
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-27 13:24:55 -0500

    Update nOGs FAQ

--- src/app/faq/nogs/page.tsx
+++ src/app/faq/nogs/page.tsx
@@ -6,7 +6,12 @@ const questions = [
   {
     question: "What are nOGs?",
     answer:
-      "nOGs are Nounspace OG NFTs. These NFTs feature hand-drawn frames by @latsko.eth, masked over 1011 different pieces of CC0 artwork and photography. Minting nOGs grants the earliest access to customize your space on Nounspace and includes free premium access for life.",
+      "[nOGs](https://highlight.xyz/mint/base:0xD094D5D45c06c1581f5f429462eE7cCe72215616) are Nounspace OG NFTs. These NFTs feature hand-drawn frames by @latsko.eth, masked over 1011 different pieces of CC0 artwork and photography. Minting nOGs grants earliest access to new features and includes free premium access for life.",
+  },
+  {
+    question: "How much do nOGs cost to mint?",
+    answer:
+      "0.0111 ETH plus a 0.0008 ETH protocol fee, or 0.0119 ETH in total.",
   },
   {
     question: "What benefits do nOG holders get?",
@@ -14,7 +19,7 @@ const questions = [
       "nOGs holders receive the earliest access to customize their space on Nounspace starting July 2nd. Additionally, they get free premium access for life, ensuring they can fully utilize all features and customizations available on the platform to their fullest extent.",
   },
   {
-    question: "Why should I mint an nOG?",
+    question: "Why should I mint nOGs?",
     answer:
       "Minting nOGs not only provides you with early access and premium features on Nounspace but also makes you part of an exclusive community. You'll have the opportunity to shape the development and future of Nounspace, and enjoy a unique and highly customizable social media experience.",
   },
